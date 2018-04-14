19e70850a1b39807c2fb663001ae1603b6337313
bhupendra-nitt <bhupendra.nitt@gmail.com>
2018-04-14 11:16:22 +0530

    Render view with pug as template engine

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'build')));
+app.set('view engine', 'pug');
+
+app.get('*', function (req, res) {
+  res.render('index');});
 
 io.on('connection', function (socket) {
   console.log('a user connected');
